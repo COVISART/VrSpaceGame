4ff2be180140d8c025bb83dbc12c155d606ab80e
Nobat <nobatgeldi@covisart.com>
2019-08-17 21:13:44 +0300

    init project

---
+++ Assets/CompleteCameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CompleteCameraController : MonoBehaviour {
+
+    public GameObject player;        //Public variable to store a reference to the player game object
+
+
+    public float offset=0.5f;            //Private variable to store the offset distance between the player and camera
+
+    // Use this for initialization
+    // LateUpdate is called after Update each frame
+    void LateUpdate () 
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        transform.position = Vector3.Lerp(transform.position, player.transform.position, offset);
+        //Vector3.Lerp(_cacheTransform.position, target.position - target.forward * distance + target.up * chaseHeight, Time.deltaTime * followDamping * chaseDistance);
+    }
+}
---
+++ Assets/NamePip.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using UnityEngine;
+using UnityEngine.UI;
+public class NamePip : MonoBehaviour
+{
+    /*
+    * public NamedPipeClientStream (
+    * string serverName, 
+    * string pipeName, 
+    * System.IO.Pipes.PipeDirection direction, 
+    * System.IO.Pipes.PipeOptions options);
+    */
+    public Transform targetObject;
+    public Text AxisX, AxisY, AxisZ;
+    public Vector3 axisData;
+    private static NamedPipeClientStream pipeClient;
+    private void Start()
+    {
+        try{ pipeClient = new NamedPipeClientStream(".", "UNITY_PIP", PipeDirection.InOut, PipeOptions.Asynchronous);}catch(Exception xpc){ Debug.LogError(xpc.Message);}
+    }
+    private void Update()
+    {
+        //axisData = new Vector3 (targetObject.rotation.x,targetObject.rotation.y,targetObject.rotation.z );
+        axisData = new Vector3 (targetObject.eulerAngles.x, targetObject.eulerAngles.y, targetObject.eulerAngles.z );
+        AxisX.text = axisData.x.ToString();
+        AxisY.text = axisData.y.ToString();
+        AxisZ.text = axisData.z.ToString();
+        //SendDataAsync(axisData.ToString(), pipeClient);
+    }
+    public bool ConnectServerAsync()
+    {
+        try
+        {
+            pipeClient.ConnectAsync(2);
+            return true;
+        }
+        catch (Exception exp)
+        {
+            throw exp;
+        }
+    }
+    public bool ConnectServer()
+    {
+        try
+        {
+            pipeClient.Connect(2);
+            return true;
+        }
+        catch (Exception exp)
+        {
+            throw exp;
+        }
+    }
+    private static string SendDataAsync(string input, NamedPipeClientStream clientStream)
+    {
+        using (var sr = new StreamWriter(clientStream))
+        {
+            try
+            {
+                sr.WriteAsync(input);
+                sr.Dispose();
+                sr.Close();
+                return "SUCCES";
+            }
+            catch (Exception exp)
+            {
+                sr.Dispose();
+                sr.Close();
+                return "WriteAsync: "+exp.Message;
+            }
+        }
+    }
+    private static string SendData(string input, NamedPipeClientStream clientStream)
+    {
+        using (var sr = new StreamWriter(clientStream))
+        {
+            try
+            {
+                sr.WriteLine(input);
+                sr.Dispose();
+                sr.Close();
+                return "SUCCES";
+            }
+            catch (Exception exp)
+            {
+                sr.Dispose();
+                sr.Close();
+                return "WriteLine: "+exp.Message;
+            }
+            
+        }
+    }
+}
