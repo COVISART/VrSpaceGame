52727585cd3731fb144993745c0ded2145ba1800
Nobat <nobatgeldi@covisart.com>
2019-09-01 06:15:05 +0300

    game input manager added for joystcik

---
+++ Assets/Scripts/InputManagerLocal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+
+
+public class InputManagerLocal : MonoBehaviour
+{
+    TrackingSpaceType trackingSpaceType;
+    void Start()
+    {
+        trackingSpaceType = TrackingSpaceType.Stationary;
+        XRDevice.SetTrackingSpaceType(trackingSpaceType);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            /*OpenVR.System.ResetSeatedZeroPose();
+            OpenVR.Compositor.SetTrackingSpace(ETrackingUniverseOrigin.TrackingUniverseSeated);*/
+            InputTracking.Recenter();
+            Debug.Log("Must be recentered");
+        }
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+            InputTracking.disablePositionalTracking = !InputTracking.disablePositionalTracking;
+    }
+}
---
+++ Assets/Scripts/SimulatorControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Luminosity.IO;
+using UnityEngine.XR;
+
+public class SimulatorControl : MonoBehaviour
+{
+    public Transform inputObject, outputObject;
+    public bool isEnabledSimulatorSyste = false;
+    TrackingSpaceType trackingSpaceType;
+
+    void Start()
+    {
+        trackingSpaceType = TrackingSpaceType.Stationary;
+        XRDevice.SetTrackingSpaceType(trackingSpaceType);
+    }
+
+    // Start is called before the first frame update
+    public void ToggleSimulatorSystem()
+    {
+        isEnabledSimulatorSyste = !isEnabledSimulatorSyste;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (InputManager.GetButtonDown("ToggleSimulatorSystem"))
+            ToggleSimulatorSystem();
+        else
+            ResetPos();
+        if (isEnabledSimulatorSyste)
+            RotationPass();
+
+        if (InputManager.GetButtonDown("ResetPosition"))
+            InputTracking.Recenter();
+        if (InputManager.GetButtonDown("disablePositionalTracking"))
+            InputTracking.disablePositionalTracking = !InputTracking.disablePositionalTracking;
+    }
+    void ResetPos()
+    {
+        outputObject.localRotation = new Quaternion();
+
+    }
+    void RotationPass()
+    {
+        outputObject.localRotation = new Quaternion(inputObject.localRotation.x, inputObject.localRotation.y, inputObject.localRotation.z, 1);
+    }
+}
